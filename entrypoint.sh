beca1b1ea0ea8b5719a7ea3230bc1ce73f7c8b72
Sagnik Sasmal <16162562+SagnikSRHUSE@users.noreply.github.com>
2020-09-08 23:03:42 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
 # Run the Server
-pip3 install -q -r requirements.txt
+/usr/local/bin/python -m pip install --upgrade pip
+/usr/local/bin/python -m pip install -q -r requirements.txt
 ${MODIFIED_STARTUP}
